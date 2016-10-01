dc354d5e637718dbc2361a3bbb2746e128a3ede1
nbkhope <ryumemaru@yahoo.com>
2016-10-01 12:58:41 -0700

    Add initial code for index ios component

---
+++ index.ios.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+
+const App = () => {
+  return (
+    <Text>The Beginning</Text>
+  );
+};
+
+AppRegistry.registerComponent('albums', () => App);
