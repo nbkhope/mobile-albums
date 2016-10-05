6ba48e79185eed7ce112269832b6f08a5f551c07
nbkhope <ryumemaru@yahoo.com>
2016-10-05 12:35:52 -0700

    Add missing import for View comp

--- index.ios.js
+++ index.ios.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 import Header from './src/components/Header';
 import AlbumList from './src/components/AlbumList';
 
