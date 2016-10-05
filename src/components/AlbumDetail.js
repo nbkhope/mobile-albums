cb70b84a96d0ae7779ded343fcd61ca694e68624
nbkhope <ryumemaru@yahoo.com>
2016-10-05 14:52:13 -0700

    Add initial Card component and use it in AlbumDetail

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import Card from './Card';
 
 const AlbumDetail = ({ album }) => {
   return (
     <View>
-      <Text>{album.title}</Text>
+      <Card>
+        <Text>{album.title}</Text>
+      </Card>
     </View>
   );
 };
