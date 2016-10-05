0a2332c4e2e359fa74a13e2744ac450822e12e9b
nbkhope <ryumemaru@yahoo.com>
2016-10-05 12:29:58 -0700

    Add AlbumList dummy component (rename header to Header.js

--- index.ios.js
+++ index.ios.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { AppRegistry } from 'react-native';
-import Header from './src/components/header';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 const App = () => {
   return (
-    <Header title={"Albums"} />
+    <View>
+      <Header title={"Albums"} />
+      <AlbumList />
+    </View>
   );
 };
 
